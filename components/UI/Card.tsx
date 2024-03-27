c721bc9d5779c55f9037e71d40c321cbab95e196
b.chirani <b.chirani@pythys.com>
2024-03-27 10:17:09 +0100

    updated Card component

--- components/UI/Card.tsx
+++ components/UI/Card.tsx
@@ -1,19 +1,22 @@
-import { View } from 'react-native';
+import { View, ViewProps } from 'react-native';
 import React, { ReactNode } from 'react';
 import { spacing, colors } from './Colors';
 
-interface CardProps {
-  children: ReactNode;
-}
-const Card = ({ children }: CardProps) => {
+interface CardProps extends ViewProps {}
+const Card = (props: CardProps) => {
+  const { children } = props;
   return (
     <View
-      style={{
-        padding: spacing.lg,
-        borderWidth: 2,
-        borderColor: colors.text,
-        borderRadius: spacing.lg,
-      }}>
+      {...props}
+      style={[
+        {
+          padding: spacing.lg,
+          borderWidth: 2,
+          borderColor: colors.text,
+          borderRadius: spacing.lg,
+        },
+        props.style,
+      ]}>
       {children}
     </View>
   );
